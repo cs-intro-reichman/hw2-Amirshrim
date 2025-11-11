58c38e0e2d216fe1d2db2753b147eda8acb4131c
Amirshrim <amirshrim@gmail.com>
2025-11-11 22:40:28 +0200

    try4

--- Cheers.java
+++ Cheers.java
@@ -6,13 +6,13 @@ public class Cheers {
         
         int num = Integer.parseInt(args[1]);
         word = word.toUpperCase();
+        
         for(int i = 0; i < word.length(); i++){
             char currentLetter = word.charAt(i); 
-        
             if (letters.indexOf(currentLetter) >= 0) { 
                 System.out.println("Give me an " + currentLetter + ": " + currentLetter + "!");
             } else {
-                System.out.println("Give me a " + currentLetter + ": " + currentLetter + "!");
+                System.out.println("Give me a  " + currentLetter + ": " + currentLetter + "!"); 
             }
         }
         
@@ -25,4 +25,4 @@ public class Cheers {
         }        
         
     } 
-} 
+}
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,32 @@
-// Demonstrates the Collatz conjecture.
+
+
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		String letter = args[1];
+		int counter=1;
+		for (int i = 1; i <= num; i++){
+			int seed=i;
+			while(seed!=1){
+				if(seed % 2==0){
+					System.out.print(seed + " ");
+					seed = seed/2;
+					
+				}
+				else{
+					System.out.print(seed + " ");
+					seed= (seed*3) + 1;
+				}
+				counter++;
+			}
+			
+			}
+			
+
+
+			
+
+		}
+
 	}
-}
+
